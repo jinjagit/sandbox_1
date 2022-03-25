63a922bdb8ff2222083e753d270d1c8d2e078b9e
Simon Tharby <simontharby@gmail.com>
2022-03-25 20:17:06 +0000

    Button toggles rotation & prints state to UI label

--- Planet.gd
+++ Planet.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+onready var StatsText = get_node("../Control/CanvasLayer/RichTextLabel")
+
 func _init():
 	VisualServer.set_debug_generate_wireframes(true)
 
@@ -20,3 +22,10 @@ func _process(delta):
 	rotate_object_local(Vector3(1, 0, 0), delta/23)
 	rotate_object_local(Vector3(0, 0, 1), delta/27)
 	
+func _on_Button_pressed():
+	set_process(not is_processing())
+	
+	if is_processing():
+		StatsText.text = str('Rotation set to ON')
+	else:
+		StatsText.text = str('Rotation set to OFF')
